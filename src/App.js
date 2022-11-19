49da74b1555c6aab9e17987b059939d60a3ccaa7
lat71 <lat71@case.edu>
2022-11-19 18:42:49 -0500

    Implemented Banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import ButtonAppBar from "./components/ButtonAppBar";
+
 function App() {
   return (
     <div className="App">
+      <ButtonAppBar />
       <h2>To Do App</h2>
     </div>
   );
---
+++ src/components/ButtonAppBar.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Icon from '@mui/material/Icon';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import "font-awesome/css/font-awesome.css";
+
+export default function ButtonAppBar() {
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+            <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ ml: 60, mr: -1 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                FRAMEWORK
+            </Typography>
+            <Button variant="contained" color="primary" startIcon={<Icon className={"fa fa-plus-circle"} />}>Add</Button>
+            </Toolbar>
+        </AppBar>
+        </Box>
+    );
+}
